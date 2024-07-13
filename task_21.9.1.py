8460ef4e0b663a604d7fd07f532121618c6d1db1
Aleksandr Kulkov <129850584+Aleksandr-Kulkov@users.noreply.github.com>
2024-07-13 21:12:25 +0300

    Update task_21.9.1.py

--- task_21.9.1.py
+++ task_21.9.1.py
@@ -4,8 +4,14 @@ class Rectangle:
         self.y = y
         self.width = width
         self.height = height
+
     def __str__(self):
         return f"Rectangle: {self.x}, {self.y}, {self.width}, {self.height}."
 
+    def get_area(self):
+        return self.width * self.height
+
+
 rect_1 = Rectangle(5, 10, 50, 100)
 print(rect_1)
+print(rect_1.get_area())
